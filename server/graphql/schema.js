20f7f30a25f8775883f25b1834f95bb281d8eb29
Garrett Johnson <garrettjtrains@gmail.com>
2022-10-12 15:57:44 -0700

    feat(server): create crud functionality for expenses

--- server/graphql/schema.js
+++ server/graphql/schema.js
@@ -3,10 +3,13 @@ import { gql } from 'apollo-server-express'
 const typeDefs = gql`
     type Query {
         getExpenses: [Expense]
+        getSingleExpense(id: String): Expense
     }
 
     type Mutation {
         addExpense(expenseInput: ExpenseInput): Expense!
+        updateExpense(expenseInput: ExpenseInput): Expense!
+        deleteExpense(expenseId: String): Expense!
     }
 `
 export default typeDefs;
